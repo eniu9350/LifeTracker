5868b4a24ab5c0a1fc284dba12c47ceac78724a7
niujin <niujin@gmail.com>
2012-03-04 12:34:04 +0800

    trival

--- gen/info/realjin/lifetracker/R.java
+++ gen/info/realjin/lifetracker/R.java
@@ -13,8 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int SerialMain_btStart=0x7f050002;
+        public static final int frameLayout1=0x7f050001;
+        public static final int linearLayout1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int start=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/info/realjin/lifetracker/LifeTrackerActivity.java
+++ src/info/realjin/lifetracker/LifeTrackerActivity.java
@@ -5,6 +5,8 @@ import info.realjin.lifetracker.persistence.DbHelper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LifeTrackerActivity extends Activity {
 
@@ -12,23 +14,29 @@ public class LifeTrackerActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.start);
 
 		// test
 		DbHelper helper = DbHelper.getHelper(this);
 		Dao dao = helper.getDao();
 
-		//init table if not exist
+		// init table if not exist
 		boolean exist = dao.isInfoExist();
 		Log.e("info exist====", String.valueOf(exist));
 		if (!exist) {
 			dao.createInfo();
 			Log.e("===complete===", "info creation");
 		}
-		
-		//
-		
-		
+
+		// test
+		Button bt = (Button) findViewById(R.id.SerialMain_btStart);
+		bt.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				
+			}
+
+		});
+
 	}
 
 }
