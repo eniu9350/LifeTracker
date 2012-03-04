e2990fee6db678e21d89ff261b864393188456bc
niujin <niujin@gmail.com>
2012-03-04 14:42:54 +0800

    tab

--- gen/info/realjin/lifetracker/R.java
+++ gen/info/realjin/lifetracker/R.java
@@ -18,11 +18,13 @@ public final class R {
         public static final int linearLayout1=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int start=0x7f030001;
+        public static final int currenttasks=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int start=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int test1=0x7f040002;
     }
 }
---
+++ src/info/realjin/lifetracker/presentation/CurrentTasksActivity.java
@@ -0,0 +1,12 @@
+package info.realjin.lifetracker.presentation;
+
+import info.realjin.lifetracker.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CurrentTasksActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.currenttasks);
+	}
+}
